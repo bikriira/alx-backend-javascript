305718399d46af072f37e30d0e3fdd6c764b001d
bikriira <bikriiradufasha@gmail.com>
2024-07-24 05:48:52 +0200

    []Achievement] do task 1(master promise's reject and resolve)

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,7 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) =>
+        success
+            ? resolve({ status: 200, body: "Success" })
+            : reject(new Error("The fake API is not working currently"))
+    );
+}
