bac83ab0179eccbb2b81962ce412468017709988
bikriira <bikriiradufasha@gmail.com>
2024-07-25 04:25:28 +0200

    [Learn] Iterators and generators

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,13 @@
+export default function createIteratorObject(report) {
+  const allEmployees = report.allEmployees;
+  
+  return {
+    *[Symbol.iterator]() {
+      for (const department in allEmployees) {
+        for (const employee of allEmployees[department]) {
+          yield employee;
+        }
+      }
+    }
+  };
+}
---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,4 @@
+Copy
+export default function iterateThroughObject(reportWithIterator) {
+  return [...reportWithIterator].join(' | ');
+}
