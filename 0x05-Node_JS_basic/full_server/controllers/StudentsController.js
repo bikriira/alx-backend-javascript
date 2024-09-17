716cbd883312b693d5ec48d20ebce53591950b09
bikriira <iradufashabikri@gmail.com>
2024-09-18 01:56:23 +0200

    [Achievement]

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,41 @@
+import readDatabase from '../utils';
+
+class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      const data = await readDatabase(process.argv[2]);
+      let responseText = 'This is the list of our students\n';
+      const categories = Object.entries(data).sort((a, b) => a[0].localeCompare(b[0]));
+
+      categories.forEach(([field, students], index) => {
+        responseText += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`;
+        if (index < categories.length - 1) {
+          responseText += '\n';
+        }
+      });
+      
+      response.status(200).send(responseText);
+    } catch (error) {
+      console.log(error);
+      response.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    const major = request.params.major;
+    if (!['CS', 'SWE'].includes(major)) {
+      return response.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const data = await readDatabase(process.argv[2]);
+      const students = data[major] || [];
+      const list = `List: ${students.join(', ')}`;
+      response.status(200).send(list);
+    } catch (error) {
+      response.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,26 @@
+import fs  from "fs/promises";
+
+export default function readDatabase(filePath) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const data = await fs.readFile(filePath, 'utf-8');
+      const lines = data
+        .trim()
+        .split('\n')
+        .filter((line) => line); // Remove empty lines
+      const students = lines.slice(1); // Remove header
+
+      const fields = {};
+      students.forEach((student) => {
+        const [firstName, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstName);
+      });
+      resolve(fields);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
