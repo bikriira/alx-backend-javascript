187eaf5eaa6b61c0c5fa8351b51cead0d9db0952
bikriira <iradufashabikri@gmail.com>
2024-09-17 22:54:43 +0200

    [Fix] handle not found routed

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,17 +3,16 @@ const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
+
   if (req.url === '/') {
     res.write('Hello Holberton School!');
     res.end();
-  }
-  if (req.url === '/students') {
-    // res.setHeader('Content-Type', 'text/plain');
+  } else if (req.url === '/students') {
     res.write('This is the list of our students\n');
     try {
       const data = await countStudents(process.argv[2]);
       console.log(process.argv[2]);
-      
+
       const categories = Object.entries(data.category);
       const { totalStudents } = data;
 
@@ -32,12 +31,15 @@ const app = http.createServer(async (req, res) => {
           res.write('\n');
         }
       }
+      res.end();
     } catch (error) {
-      // res.statusCode = 501;
-      res.write(error);
+      res.statusCode = 501;
+      res.end(error.message);
     }
+  } else {
+    res.statusCode = 404;
+    res.end('Not found');
   }
-  res.end();
 });
 app.listen(1245);
 
