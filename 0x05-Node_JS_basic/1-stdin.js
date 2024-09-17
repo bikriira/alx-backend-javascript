c649cf10e46bcc0bcbd373a24698a75a501a1b17
bikriira <iradufashabikri@gmail.com>
2024-09-17 13:07:43 +0200

    [Fix] use the eslint to lint(runned sudo apt remove eslint)

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
+  input: process.stdin,
+  output: process.stdout,
 });
 
 rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-    console.log(`Your name is: ${answer}`);
-    rl.close();
+  console.log(`Your name is: ${answer}`);
+  rl.close();
 });
 
 rl.on('close', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
