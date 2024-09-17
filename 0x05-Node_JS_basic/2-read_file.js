af9b43ed624e98003bafd623576694cb0bc5a0c9
bikriira <iradufashabikri@gmail.com>
2024-09-17 15:25:37 +0200

    [Fix] do task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data
+      .trim()
+      .split('\n')
+      .filter((line) => line); // Remove empty lines
+    const students = lines.slice(1); // Remove header
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+    students.forEach((student) => {
+      const [firstName, , , field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(firstName);
+    });
+
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(
+        `Number of students in ${field}: ${
+          students.length
+        }. List: ${students.join(', ')}`,
+      );
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
